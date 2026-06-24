2f82ba8b43b78f6d628f725fcf80bbe77cd12db7
plasidius <plasidius7@gmail.com>
2026-06-24 14:15:55 +0700

    fix: lengkapi CSP untuk GoPay/Google Pay Midtrans

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,9 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://app.sandbox.midtrans.com https://snap-assets.sandbox.midtrans.com https://www.google.com https://www.gstatic.com https://recaptcha.net",
-              "frame-src 'self' https://app.sandbox.midtrans.com https://www.google.com https://recaptcha.net",
-              "connect-src 'self' https://api.sandbox.midtrans.com https://*.supabase.co wss://*.supabase.co https://www.google.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://app.sandbox.midtrans.com https://snap-assets.sandbox.midtrans.com https://api.sandbox.midtrans.com https://pay.google.com https://gwk.gopayapi.com https://www.google.com https://www.gstatic.com https://recaptcha.net",
+              "frame-src 'self' https://app.sandbox.midtrans.com https://pay.google.com https://www.google.com https://recaptcha.net",
+              "connect-src 'self' https://api.sandbox.midtrans.com https://pay.google.com https://gwk.gopayapi.com https://*.supabase.co wss://*.supabase.co https://www.google.com",
               "img-src 'self' data: https:",
               "style-src 'self' 'unsafe-inline'",
               "font-src 'self' data:",
