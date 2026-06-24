338f6a52043ee6661a04668b926363063b0394b4
plasidius <plasidius7@gmail.com>
2026-06-24 14:28:35 +0700

    feat: support sandbox/production Midtrans via env var

--- lib/midtrans.ts
+++ lib/midtrans.ts
@@ -1,7 +1,7 @@
 const MidtransClient = require('midtrans-client');
 
 export const snap = new MidtransClient.Snap({
-  isProduction: false,
+  isProduction: process.env.NEXT_PUBLIC_MIDTRANS_ENV === "production",
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
